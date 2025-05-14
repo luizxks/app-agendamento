6216b38c046b757b495437d026756499099d03e8
Luiz Felipe <luizfelipp9@gmail.com>
2025-05-13 20:32:20 -0400

    implementada tela-tosca

--- src/storage/async-storage.js
+++ src/storage/async-storage.js
@@ -3,11 +3,20 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const key = 'tasks';
 
 const getData = async () => {
-    return await AsyncStorage.getItem();
+    // JSON.parse transforma textos em objetos
+    let data = JSON.parse(await AsyncStorage.getItem(key));
+
+    // Se não houverem dados, preencher e salvar um array vazio
+    if (data == null) {
+        data = []
+        setData(data)
+    }
+    return data
 }
 
-const setData = async (key, value) => {
-    await AsyncStorage.setItem(key, JSON.stringify(value))
+const setData = async (value) => {
+    // JSON.stringify transforma objetos em textos
+    await AsyncStorage.setItem(key, JSON.stringify(value));
 }
 
 export {
